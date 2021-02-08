cd9dd72fe1be38fb0d120b599efa3588cad1810b
Hershel Wathore <wathoreh@gmail.com>
2021-02-07 20:23:20 -0500

    popup with the link shows up

--- extension/public/app/core.js
+++ extension/public/app/core.js
@@ -1,20 +1,20 @@
 // Unique ID for the className.
 var MOUSE_VISITED_CLASSNAME = 'crx_mouse_visited';
 
+// Previous dom, that we want to track, so we can remove the previous styling.
+var prevDOM = null;
+
 // Mouse listener for any move event on the current document.
 document.addEventListener('mousemove', function (e) {
   var srcElement = e.srcElement;
-
   // Lets check if our underlying element is a DIV.
-  if (srcElement.nodeName == 'A') {
-    var url = srcElement.getAttribute("href");
-    
-
-    // Add a visited class name to the element. So we can style it.
-    srcElement.classList.add(MOUSE_VISITED_CLASSNAME);
-
-    // The current element is now the previous. So we can remove the class
-    // during the next iteration.
-    prevDOM = srcElement;
+  while (srcElement != null && srcElement.getAttribute("data-pagelet") != 'FeedUnit_1') {
+    if (srcElement.nodeName == 'A') {
+      var url = srcElement.getAttribute("href");
+      alert(url);
+    }
+    srcElement = srcElement.parentNode;
+    console.log(srcElement);
   }
+  console.log(srcElement)
 }, false);
