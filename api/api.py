db4f334122b38e2c067f14f6c25e6cfa1f4a627e
zbilcheck <zbilcheck@DESKTOP-DTVAGQM.localdomain>
2021-02-08 21:53:31 -0500

    Fixing Urie's Mess

--- api/api.py
+++ api/api.py
@@ -36,6 +36,8 @@ def process_url():
 @app.after_request
 def apply_header(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = "GET,POST,PUT,DELETE,OPTIONS"
+    response.headers['Access-Control-Allow-Headers'] = "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
     return response
 
 if __name__ == '__main__':
