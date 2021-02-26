f3faa9e4fa21e0a64b4ff4cbca3e2558e55d204d
bruce <Bruce.J.Zou.21@Dartmouth.edu>
2021-02-26 02:37:56 -0500

    Add searching for google news for articles

---
+++ api/news_search.py
@@ -0,0 +1,86 @@
+import tldextract
+from pygooglenews import GoogleNews
+from datetime import timedelta
+from tinydb import Query
+
+DATE_FORMAT = "%Y-%m-%d"
+
+def google_news_search(keywords, date, num_suggestions, sources, date_margin, bias_db):
+    """
+    Searches google news for `num_suggestions` articles about `keywords` around `date` from the `sources`.
+
+    Arguments
+    ----------
+    keywords: ([string]), list of keywords to search for.
+    date: (datetime), date to search around. (Can be none, if so don't pass a date to search)
+    num_suggestions: (int), number of articles to find.
+    sources: ([string]), list of news sources to find articles from.
+    bias_db: (TinyDB), media bias db.
+    """
+
+    # build query: keywords + inurl:source1 OR inurl:source2 OR etc...
+    # ex: https://news.google.com/rss/search?q=Boeing+inurl:cnn OR inurl:nytimes OR inurl:washingtonpost OR inurl:foxnews&ceid=US:en&hl=en-US&gl=US
+    # date, from_, to_, +- 1 week? configurable
+    gn = GoogleNews()
+
+    query_str = create_query_str(keywords, sources)
+    start_date, end_date = None, None
+    if date:
+        start_date, end_date = get_date_range(date, date_margin)
+
+    search = gn.search(query_str, from_=start_date, to_=end_date)
+    
+    # Go through search results to get article urls to return
+    entries = search['entries']
+    if sources:
+        results = []
+        already_chosen = set()
+        for entry in entries:
+            entry_domain_name = tldextract.extract(entry.source['href']).domain
+            if entry_domain_name in sources and entry_domain_name not in already_chosen:
+                already_chosen.add(entry_domain_name)
+                results.append(entry.link)
+
+    else: # if no sources given, just get first num_suggestion articles that have source in bias_db
+        results = []
+        Media = Query()
+        for entry in entries:
+            entry_domain_name = tldextract.extract(entry.source['href']).domain
+            if bias_db.search(Media.domain_name == entry_domain_name):
+                results.append(entry.link)
+            if len(results) == num_suggestions:
+                break
+
+    return results
+
+def create_query_str(keywords, sources):
+    """
+    Creates the query string to search for `keywords` from `sources`.
+
+    Arguments
+    ----------
+    keywords: ([string]), list of keywords to search for.
+    sources: ([string]), list of news sources to find articles from.
+    """
+    str_builder = keywords.copy()
+    for source in sources:
+        str_builder.append('inurl:' + source)
+        str_builder.append('OR')
+    if str_builder[-1] == 'OR':
+        del str_builder[-1]
+    
+    return ' '.join(str_builder)
+
+def get_date_range(date, margin):
+    """
+    Returns the start and end date range for date-margin to date+margin as strings in DATE_FORMAT.
+
+    Arguments
+    ----------
+    date: (datetime), a date time.
+    margin: (int), margin of time (in days) to add and subtract from date.
+    """
+    time_delta = timedelta(days=margin)
+    start_date = date - time_delta
+    end_date = date + time_delta
+    return start_date.strftime(DATE_FORMAT), end_date.strftime(DATE_FORMAT)
--- api/suggestions.py
+++ api/suggestions.py
@@ -1,5 +1,8 @@
+import random
+from bias import Bias, get_bias
+from news_search import google_news_search
 
-def get_suggested_articles(article_url, num_suggestions, trusted_sources):
+def get_suggested_articles(article_url, num_suggestions, app_config):
     """
     Gets a list of articles related to the given article.
 
@@ -7,12 +10,25 @@ def get_suggested_articles(article_url, num_suggestions, trusted_sources):
     ----------
     article_url: (string), the url of the original article.
     num_suggestions: (int), the number of related articles to find.
+    app_config: (flask app.config), used to get other config variables.
     """
-    article_text = get_article_text(article_url)
+    trusted_sources = app_config['TRUSTED_SOURCES']
+    top_n_keywords = app_config['TOP_N_KEYWORDS']
+    date_margin = app_config['DATE_MARGIN']
+
+    article_text, article_date = get_article_text(article_url)
     article_keywords = get_article_keywords(article_text)
-    related_articles = get_related_articles(article_keywords, num_suggestions, trusted_sources)
-    
-    return [] 
+    related_articles = get_related_articles(article_keywords, article_date, num_suggestions, trusted_sources, top_n_keywords, date_margin, app_config['MEDIA_BIAS_DB'])
+
+    # add biases
+    suggested_articles = []
+    for article in related_articles:
+        bias = get_bias(article, app_config['MEDIA_BIAS_DB'])
+        suggestion = {'bias': bias, 'article_url': article}
+        suggested_articles.append(suggestion)
+
+    return suggested_articles 
+
 
 def get_article_text(article_url):
     """
@@ -25,6 +41,7 @@ def get_article_text(article_url):
 
     return ""
 
+
 def get_article_keywords(article_text):
     """
     Gets the keywords or phrases of the article returned as a list of strings.
@@ -36,15 +53,28 @@ def get_article_keywords(article_text):
 
     return []
 
-def get_related_articles(article_keywords, num_suggestions, trusted_sources):
+def get_related_articles(article_keywords, article_date, num_suggestions, trusted_sources, top_n_keywords, date_margin, bias_db):
     """
-    Gets a list of related articles (length = num_suggestions) from trusted sources based on the article keywords.
+    Gets a list of related articles (url) (length = num_suggestions) from trusted sources based on the article keywords.
 
     Arguments
     ----------
     article_keywords: ([string]), list of keywords defining the article.
     num_suggestions: (int), number of related articles to find.
     trusted_sources: ([string]), list of trusted news sources to find related articles from.
+    top_n_keywords: (int), number of keywords to keep.
+    date_margin: (int), number of days to +- from the article date for searching.
     """
+    suggested_articles = []
+    chosen_sources = []
 
-    return []
+    if trusted_sources:
+        if num_suggestions > len(trusted_sources): # wants more suggestions than trusted sources, just return from all sources
+            num_suggestions = len(trusted_sources)
+        chosen_sources = random.sample(trusted_sources, num_suggestions) # randomly choose some sources
+
+    selected_keywords = article_keywords[:top_n_keywords] # Get the top_n_keywords to search with
+    related_articles = google_news_search(selected_keywords, article_date, num_suggestions, chosen_sources, date_margin, bias_db)
+
+    return related_articles
+
