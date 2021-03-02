64f96f3736a66cc07e2a14952c4c00d33a62fffa
Hershel Wathore <wathoreh@gmail.com>
2021-03-02 18:39:56 -0500

    made some changes

--- extension/public/app/core.js
+++ extension/public/app/core.js
@@ -25,38 +25,40 @@
 //   }
 // }, false);
 
-var feed2 = document.querySelector('[role="main"]');
-var observer2 = new MutationObserver(function(mutations) {
-  var feed = document.querySelectorAll('[role="main"]');
-  console.log(feed);
-  var posts2 = Array.prototype.slice.call(feed);
-  console.log("side pages", posts2);
-  if (feed.length == 2) {
-    var innerhtml = feed.item(1).childNodes.item(0);
-    console.log(innerhtml);
-    innerhtml.childNodes.forEach((post) => {
-      var menu = post.querySelector('[aria-haspopup="menu"]');
-      if ( menu.parentElement.parentElement.querySelector('[class="true_img"]') == null ) {
-  
-        var elem = document.createElement("img");
-        elem.src = chrome.extension.getURL("assets/True Pill - UI Icon.png");
-        elem.style.paddingTop = "12px";
-        elem.setAttribute("class", "true_img");
-        menu.parentElement.parentElement.appendChild(elem); 
-        elem.onclick = function() { getBias('http://localhost:5000/api/truepill/', post); }  
-      }
+// var feed2 = document.querySelector('[data-pagelet="page"]');
+// console.log(feed2)
+// var observer2 = new MutationObserver(function(mutations) {
+//   var posts2 = document.querySelectorAll('[role="article"]');
+//   console.log(posts2);
 
-    });
-  }
-});
-var config = { attributes: true, childList: true, characterData: true };
-observer2.observe(feed2, config);
+//   posts.forEach(post => {
+//     var menu = post.querySelector('[aria-haspopup="menu"]');
+//     if ( menu.parentElement.parentElement.querySelector('[class="true_img"]') == null ) {
 
-var feed = document.querySelector('[role="feed"]');
+//       var elem = document.createElement("img");
+//       elem.src = chrome.extension.getURL("assets/True Pill - UI Icon.png");
+//       elem.style.paddingTop = "12px";
+//       elem.setAttribute("class", "true_img");
+//       menu.parentElement.parentElement.appendChild(elem); 
+//       elem.onclick = function() { getBias('http://localhost:5000/api/truepill/', post); }
+ 
+//     }
+    
+//   });
+
+// });
+// var config = { attributes: true, childList: true, characterData: true };
+// observer2.observe(feed2, config);
 
 var observer = new MutationObserver(function(mutations) {
-  var posts = document.querySelectorAll('[data-pagelet^="FeedUnit_"]');
-  console.log(posts)
+  // var posts = document.querySelectorAll('[data-pagelet^="FeedUnit_"]');
+  window.setTimeout(null,500);
+  var posts = document.querySelectorAll('[role="article"]');
+  while (!posts){
+    window.setTimeout(null,50);
+    var posts = document.querySelectorAll('[role="article"]');
+  }
+  console.log("posts", posts);
   posts.forEach(post => {
     var menu = post.querySelector('[aria-haspopup="menu"]');
     if ( menu.parentElement.parentElement.querySelector('[class="true_img"]') == null ) {
@@ -74,8 +76,24 @@ var observer = new MutationObserver(function(mutations) {
   
 });
 
-var config = { attributes: true, childList: true, characterData: true };
-observer.observe(feed, config);
+function addObserverIfDesiredNodeAvailable() {
+  var feed = document.querySelector('[role="feed"]');
+  if (!feed){
+    feed = document.querySelector('[data-pagelet="page"]');
+    if(!feed) {
+      //The node we need does not exist yet.
+      //Wait 50ms and try again
+      console.log("timeout");
+      window.setTimeout(addObserverIfDesiredNodeAvailable,50);
+      return;
+    }
+  }
+  console.log("found feed", feed);
+  var config = { attributes: true, childList: true, characterData: true };
+  observer.observe(feed, config);
+}
+
+addObserverIfDesiredNodeAvailable();
 
 function getBias(serv_url, post) {
   var fb_url = null;
