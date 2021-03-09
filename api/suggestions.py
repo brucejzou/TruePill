f57ce2cc4711452976c4ef4a17270b17699d4a36
bruce <Bruce.J.Zou.21@Dartmouth.edu>
2021-03-08 19:54:45 -0500

    Update way of doing web requests and getting article text

--- api/suggestions.py
+++ api/suggestions.py
@@ -1,23 +1,26 @@
 import random
-import requests
 import datefinder
 import articleDateExtractor
 import string
 from bias import Bias, get_bias
 from news_search import google_news_search
+from make_requests import make_request, get_random_header
+from goose3 import Goose
+from cleantext import clean
+from dateutil import parser
 from rake_nltk import Metric, Rake
 from bs4 import BeautifulSoup
 from bs4.element import Comment
 
-TEXT_LENGTH_FILTER = 80
-HTML_BLACKLIST = [
-    'style', 
-    'script', 
-    'head', 
-    'title', 
-    'meta', 
-    '[document]'
-]
+# TEXT_LENGTH_FILTER = 80
+# HTML_BLACKLIST = [
+#     'style', 
+#     'script', 
+#     'head', 
+#     'title', 
+#     'meta', 
+#     '[document]'
+# ]
 
 def get_suggested_articles(article_url, num_suggestions, app_config):
     """
@@ -33,10 +36,11 @@ def get_suggested_articles(article_url, num_suggestions, app_config):
     top_n_keywords = app_config['TOP_N_KEYWORDS']
     date_margin = app_config['DATE_MARGIN']
 
-    article_text = get_article_text(article_url)
-    article_date = articleDateExtractor.extractArticlePublishedDate(article_url)
+    article_text, article_date, article_url = get_article_text_and_date(article_url)
+    # article_text = get_article_text(article_url)
+    # article_date = articleDateExtractor.extractArticlePublishedDate(article_url)
     article_keywords = get_article_keywords(article_text)
-    related_articles = get_related_articles(article_keywords, article_date, num_suggestions, trusted_sources, top_n_keywords, date_margin, app_config['MEDIA_BIAS_DB'])
+    related_articles = get_related_articles(article_url, article_keywords, article_date, num_suggestions, trusted_sources, top_n_keywords, date_margin, app_config['MEDIA_BIAS_DB'])
 
     # add biases
     suggested_articles = []
@@ -47,38 +51,48 @@ def get_suggested_articles(article_url, num_suggestions, app_config):
 
     return suggested_articles 
 
-def tag_visible(element):
-    if element.parent.name in HTML_BLACKLIST:
-        return False
-    if isinstance(element, Comment):
-        return False
-    return True
+def get_article_text_and_date(article_url):
+    g = Goose({'http_headers': get_random_header()})
+    article = g.extract(url=article_url)
+    title = article.title
+    body = clean(article.cleaned_text, no_line_breaks=True, no_punct=True)
+    date = parser.parse(article.publish_date)
+    text = clean(title, no_punct=True) + body
 
-def tag_length_filter(element):
-    if len(str(element)) < TEXT_LENGTH_FILTER:
-        return False
-    return True
+    return text, date, article.canonical_link
 
-def get_article_text(article_url):
-    """
-    Gets the article text from the given article url as a string.
+# def tag_visible(element):
+#     if element.parent.name in HTML_BLACKLIST:
+#         return False
+#     if isinstance(element, Comment):
+#         return False
+#     return True
 
-    Arguments
-    ----------
-    article_url: (string), the url of the original article.
-    """
-    result = requests.get(article_url)
+# def tag_length_filter(element):
+#     if len(str(element)) < TEXT_LENGTH_FILTER:
+#         return False
+#     return True
 
-    if result.status_code == 200:
-        content = result.content
-        soup = BeautifulSoup(content, features='html.parser')
-        texts = soup.find_all(text=True)
-        visible_texts = filter(tag_visible, texts)
-        visible_texts_long = filter(tag_length_filter, visible_texts)
-        article_text = ' '.join(t.strip() for t in visible_texts_long)
-        article_text = ''.join(filter(lambda x: x in string.printable, article_text)) # remove weird characters like the fancy quotes
-        article_text = article_text.translate(str.maketrans('', '', string.punctuation)) # remove punctuation
-        return article_text
+# def get_article_text(article_url):
+#     """
+#     Gets the article text from the given article url as a string.
+
+#     Arguments
+#     ----------
+#     article_url: (string), the url of the original article.
+#     """
+#     result = make_request(article_url)
+
+#     if result.status_code == 200:
+#         content = result.content
+#         soup = BeautifulSoup(content, features='html.parser')
+#         texts = soup.find_all(text=True)
+#         visible_texts = filter(tag_visible, texts)
+#         visible_texts_long = filter(tag_length_filter, visible_texts)
+#         article_text = ' '.join(t.strip() for t in visible_texts_long)
+#         article_text = ''.join(filter(lambda x: x in string.printable, article_text)) # remove weird characters like the fancy quotes
+#         article_text = article_text.translate(str.maketrans('', '', string.punctuation)) # remove punctuation
+#         return article_text
 
 def get_article_keywords(article_text):
     """
@@ -94,17 +108,20 @@ def get_article_keywords(article_text):
 
     return r.get_ranked_phrases()
 
-def get_related_articles(article_keywords, article_date, num_suggestions, trusted_sources, top_n_keywords, date_margin, bias_db):
+def get_related_articles(article_url, article_keywords, article_date, num_suggestions, trusted_sources, top_n_keywords, date_margin, bias_db):
     """
     Gets a list of related articles (url) (length = num_suggestions) from trusted sources based on the article keywords.
 
     Arguments
     ----------
+    article_url: (string), url of the original articl
     article_keywords: ([string]), list of keywords defining the article.
+    article_date: (datetime), date to search around. (Can be none)
     num_suggestions: (int), number of related articles to find.
     trusted_sources: ([string]), list of trusted news sources to find related articles from.
     top_n_keywords: (int), number of keywords to keep.
     date_margin: (int), number of days to +- from the article date for searching.
+    bias_db: (TinyDB), media bias db.
     """
     suggested_articles = []
     chosen_sources = []
@@ -115,6 +132,6 @@ def get_related_articles(article_keywords, article_date, num_suggestions, truste
         chosen_sources = random.sample(trusted_sources, num_suggestions) # randomly choose some sources
 
     selected_keywords = article_keywords[:top_n_keywords] # Get the top_n_keywords to search with
-    related_articles = google_news_search(selected_keywords, article_date, num_suggestions, chosen_sources, date_margin, bias_db)
+    related_articles = google_news_search(article_url, selected_keywords, article_date, num_suggestions, chosen_sources, date_margin, bias_db)
 
     return related_articles
