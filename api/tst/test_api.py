7084fc1bf824f4c1e4d20b5f948a88d9b6814904
bruce <Bruce.J.Zou.21@Dartmouth.edu>
2021-05-13 22:57:53 -0400

    Update test_api.py

--- api/tst/test_api.py
+++ api/tst/test_api.py
@@ -18,7 +18,21 @@ def test_process_url_not_request_json(client):
     pass
 
 def test_process_url_not_fb_url(client):
-    pass
+    rv = client.post('/api/truepill/', json={
+    "article_url" : "https://www.bloomberg.com/graphics/2021-covid-race-and-recovery/?srnd=premium"
+    })
+    assert rv.status_code == 200
+    json_data = rv.get_json()
+    assert 'article_url' in json_data and json_data['article_url'] is not None, "article_url not in response"
+    assert 'bias' in json_data and json_data['bias'] is not None, "bias not in response"
+    assert 'suggested_articles' in json_data, "suggested_articles not in response"
+    suggestions = json_data['suggested_articles']
+    assert len(suggestions) >= 1, "no suggestions in response"
+    first_suggestion = suggestions[0]
+    assert 'article_title' in first_suggestion and first_suggestion['article_title'] is not None, 'article_title not in suggested article data'
+    assert 'article_url' in first_suggestion and first_suggestion['article_url'] is not None, 'article_url not in suggested article data'
+    assert 'bias' in first_suggestion and first_suggestion['bias'] is not None, 'bias not in suggested article data'
+    
 
 def test_process_url_fb_url(client):
     # chloe
