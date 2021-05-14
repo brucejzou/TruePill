42c28b47e812ace6538ce8d6bc6935421309ba87
bruce <Bruce.J.Zou.21@Dartmouth.edu>
2021-05-13 22:31:05 -0400

    Update test_api.py

--- api/tst/test_api.py
+++ api/tst/test_api.py
@@ -7,3 +7,19 @@ def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
+
+
+def test_process_url_no_article_url(client):
+    rv = client.post('/api/truepill/', json={})
+    assert rv.status_code == 500
+
+def test_process_url_not_request_json(client):
+    # chloe
+    pass
+
+def test_process_url_not_fb_url(client):
+    pass
+
+def test_process_url_fb_url(client):
+    # chloe
+    pass
